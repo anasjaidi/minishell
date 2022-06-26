21b00e1f92fa8b03fbc0e405e578a54eed25d63a
anas jaidi <anas.jaidi@icloud.com>
2022-06-26 16:55:14 +0100

    add pwd cmd function

--- ex/exit.c
+++ ex/exit.c
@@ -6,7 +6,7 @@
 /*   By: ajaidi <ajaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 16:37:13 by ajaidi            #+#    #+#             */
-/*   Updated: 2022/06/26 16:51:41 by ajaidi           ###   ########.fr       */
+/*   Updated: 2022/06/26 16:54:30 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,20 @@ int	ft_exit(int status)
 {
 	ft_collect(&g.adrs, g.adrs);
 	exit(status);
+}
+
+int	ft_pwd(int fd)
+{
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (pwd)
+	{
+		ft_putstr_fd(pwd, fd);
+		ft_putchar_fd('\n', fd);
+		free(pwd);
+		exit(0);
+	}
+	else
+		exit (-1);
 }
