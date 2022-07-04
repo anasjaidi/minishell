47dc6509cae84a1978fb94769fc80e7899f2965f
anas jaidi <anas.jaidi@icloud.com>
2022-07-04 19:31:39 +0100

    finish export

--- ex/export.c
+++ ex/export.c
@@ -6,7 +6,7 @@
 /*   By: ajaidi <ajaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 22:48:58 by ajaidi            #+#    #+#             */
-/*   Updated: 2022/07/04 16:29:21 by ajaidi           ###   ########.fr       */
+/*   Updated: 2022/07/04 19:30:37 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ char **ft_split_exp(char *line)
 	char **argv, i = -1;
 
 	argv = ft_split(line, '=');
-	// if (argv[2])
+	// if (argv && argv[1] && argv[2])
 	// 	return NULL;
-	// else if (!(argv[0][0] >= '0' && argv[0][0] <= '9'))
-	// 	return NULL;
-	// while (*argv[++i])
-	// {
-	// 	if (*argv[i] == '+' && *argv[i + 1])
-	// 		return NULL;
-	// 	else if (!(*argv[i] >= 'a' && *argv[i] <= 'z') && !(*argv[i] >= 'A' && *argv[i] <= 'Z') && !(*argv[i] >= '0' && *argv[i] <= '9') && (*argv[i] != '_' && *argv[i] != '+'))
-	// 		return NULL;
-	// }
+	if ((argv[0][0] >= '0' && argv[0][0] <= '9'))
+		return NULL;
+	while (argv[0][++i])
+	{
+		if (argv[0][i] == '+' && argv[0][i] && !argv[1])
+			return NULL;
+		else if (!(argv[0][i] >= 'a' && argv[0][i] <= 'z') && !(argv[0][i] >= 'A' && argv[0][i] <= 'Z') && !(argv[0][i] >= '0' && argv[0][i] <= '9') && (argv[0][i] != '_' && argv[0][i] != '+'))
+			return NULL;
+	}
 	return argv;
 }
 
@@ -50,26 +50,33 @@ void	ft_export(char **argv)
 {
 	int i = 0;
 	char	**spl;
+	t_env *node;
 	if (!argv[1])
 		return	(void)display_exports();
 	while(argv[++i])
 	{
-		spl = ft_split(argv[i], '=');
-		// spl = ft_split_exp(argv[i]);
-		// if (!spl)
-		// {
-		// 	ft_putstr_fd("bash: export: ", 1);
-		// 	ft_putstr_fd(argv[i], 1);
-		// 	ft_putstr_fd(": not a valid identifier", 1);
-		// 	break ;
-		// }
-		// if (spl[0][ft_strlen(spl[0])-2] == '+')
-		// {
-		// 	spl[0][ft_strlen(spl[0])-2] = 0;
-		// 	t_env *node;
-		// 	node = get_env_node(spl[0]);
-		// 	node->value = ft_strjoin(node->value, spl[1]);
-		// }
-		append_in_end_env(&g.env, spl[0], spl[1]);
+		spl = ft_split_exp(argv[i]);
+		if (!spl)
+		{
+			ft_putstr_fd("bash: export: ", 1);
+			ft_putstr_fd(argv[i], 1);
+			ft_putstr_fd(": not a valid identifier\n", 1);
+			break ;
+		}
+		if (spl[0][ft_strlen(spl[0])-1] == '+')
+		{
+			spl[0][ft_strlen(spl[0])-1] = 0;
+			node = get_env_node(spl[0]);
+			if (node)
+				node->value = ft_strjoin(node->value, spl[1]);
+			else
+				append_in_end_env(&g.env, spl[0], spl[1]);
+			break ;
+		}
+		node = get_env_node(spl[0]);
+		if (!node)
+			append_in_end_env(&g.env, spl[0], spl[1]);
+		else
+			node->value = spl[1];
 	}
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ajaidi <ajaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 21:12:13 by ajaidi            #+#    #+#             */
-/*   Updated: 2022/07/03 21:15:50 by ajaidi           ###   ########.fr       */
+/*   Updated: 2022/07/04 19:16:08 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,15 @@ char	*ft_strjoin(char  *s1, char  *s2)
 	int		i2;
 	char	*ptr;
 
-	if (!s1 || !s2)
+	if (!s2)
 		return (NULL);
 	i1 = ft_strlen(s1);
 	i2 = ft_strlen(s2);
 	ptr = (char *)malloc((i1 + i2) * sizeof(char) + 1);
 	if (!ptr)
 		return (NULL);
-	ft_memmove(ptr, s1, i1);
+	if (s1)
+		ft_memmove(ptr, s1, i1);
 	ft_memmove(ptr + i1, s2, i2 + 1);
 	ptr[i1 + i2] = 0;
 	return (ptr);
